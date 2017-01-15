b1ca48b9750cc59b99d92565aac98d4255c1ade6
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2017-01-15 08:33:27 +0000

    Completed input and database insertion

--- index.php
+++ index.php
@@ -2,23 +2,57 @@
 
 	require_once("./private/init.php");
 	
-	$submitted = isset($_POST['submit']);
+	$submitted = $_SERVER['REQUEST_METHOD'] == 'POST';
+	$error = false;
 	
-	$shield_quants = array(
-		// TODO
-	);
-	
-	$armor_quants = array(
-		// TODO
+	$stats = array(
+		'intact_armor_plates' => 	(isset($_POST['plates']) && $submitted?$_POST['plates']:0),
+		'nanite_compound' =>		(isset($_POST['nanite']) && $submitted?$_POST['nanite']:0),
+		'interface_circuit' => 		(isset($_POST['interf']) && $submitted?$_POST['interf']:0),
+		'power_circuit' => 			(isset($_POST['powerc']) && $submitted?$_POST['powerc']:0),
+		'logic_circuit' => 			(isset($_POST['logicc']) && $submitted?$_POST['logicc']:0),
+		'enhanced_ward_console' => 	(isset($_POST['consol']) && $submitted?$_POST['consol']:0),
+		'shield_quant' => 			(isset($_POST['shield_quant']) && $submitted?$_POST['shield_quant']:0),
+		'shield_price' => 			(isset($_POST['shield_price']) && $submitted?$_POST['shield_price']:0),
+		'armor_quant' => 			(isset($_POST['armor_quant']) && $submitted?$_POST['armor_quant']:0),
+		'armor_price' => 			(isset($_POST['armor_price']) && $submitted?$_POST['armor_price']:0)
 	);
 	
 	if($submitted){
 		
+		$error = !validate_stats($stats);
 		
+		if(!$error){
+			//No errors. Proceed
+			$date = date('Y-m-d H:i:s');
+				
+			$sql =  'INSERT INTO rig_log ';
+			$sql .= '(date_completed, intact_armor_plates, nanite_compound, interface_circuit, power_circuit, logic_circuit, enhanced_ward_console, ';
+			$sql .= 'shield_quant, shield_price, armor_quant, armor_price)';
+			$sql .= ' VALUES ';
+			$sql .= '(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
+			$sql = $db->prepare($sql);
+			
+			$sql->bind_param('sddddddidid', 
+				$date, 
+				$stats['intact_armor_plates'],
+				$stats['nanite_compound'],
+				$stats['interface_circuit'],
+				$stats['power_circuit'],
+				$stats['logic_circuit'],
+				$stats['enhanced_ward_console'],
+				$stats['shield_quant'],
+				$stats['shield_price'],
+				$stats['armor_quant'],
+				$stats['armor_price']
+				);
+			
+			$result = $sql->execute();
+			
+		}
 		
 	}
 	
-
 ?>
 
 <html>
@@ -35,23 +69,20 @@
 			<tr>
 				<td id="secondary_header">Material Prices:</td>
 			</tr>
-		
 			<tr>
-				<td>Mat 1 (Plates): </td> <td><input type="number" step=0.01 name="plates" value=0.0></td>
-				<td>Mat 2 (Nanite): </td> <td><input type="number" step=0.01 name="nanite" value=0.0></td>
-				<td>Mat 3 (Interf): </td> <td><input type="number" step=0.01 name="interf" value=0.0></td>
+				<td>Mat 1 (Plates): </td> <td><input type="number" step=0.01 name="plates" value=<?php echo isset($stats['intact_armor_plates'])?$stats['intact_armor_plates']:0.0; ?>></td>
+				<td>Mat 2 (Nanite): </td> <td><input type="number" step=0.01 name="nanite" value=<?php echo isset($stats['nanite_compound'])?$stats['nanite_compound']:0.0; ?>></td>
+				<td>Mat 3 (Interf): </td> <td><input type="number" step=0.01 name="interf" value=<?php echo isset($stats['interface_circuit'])?$stats['interface_circuit']:0.0; ?>></td>
 			</tr>
-			
 			<tr>
-				<td>Mat 4 (PowerC): </td> <td><input type="number" step=0.01 name="powerc" value=0.0></td>
-				<td>Mat 5 (LogicC): </td> <td><input type="number" step=0.01 name="logicc" value=0.0></td>
-				<td>Mat 6 (Consol): </td> <td><input type="number" step=0.01 name="consol" value=0.0></td>
+				<td>Mat 4 (PowerC): </td> <td><input type="number" step=0.01 name="powerc" value=<?php echo isset($stats['power_circuit'])?$stats['power_circuit']:0.0; ?>></td>
+				<td>Mat 5 (LogicC): </td> <td><input type="number" step=0.01 name="logicc" value=<?php echo isset($stats['logic_circuit'])?$stats['logic_circuit']:0.0; ?>></td>
+				<td>Mat 6 (Consol): </td> <td><input type="number" step=0.01 name="consol" value=<?php echo isset($stats['enhanced_ward_console'])?$stats['enhanced_ward_console']:0.0; ?>></td>
 			<tr>
 	
 		</table>
 	
 		<div id="note">Material quantities are automatically calculated using preset values.</div>
-	
 		<br>
 	
 		<table>
@@ -59,19 +90,13 @@
 			<tr>
 				<td id="secondary_header">Product prices and quantities:</td>
 			</tr>
-	
 			<tr>
-			
-				<td>Shield quantity: <input type="number" name="shield_quant" value=0></td>
-				<td>Shield price: <input type="number" step=0.01 name="shield_price" value=0.0></td>
-			
+				<td>Shield quantity: <input type="number" name="shield_quant" value=<?php echo isset($stats['shield_quant'])?$stats['shield_quant']:0; ?>></td>
+				<td>Shield price: <input type="number" step=0.01 name="shield_price" value=<?php echo isset($stats['shield_price'])?$stats['shield_price']:0.0; ?>></td>
 			</tr>
-				
 			<tr>
-				
-				<td>Armor quantity: <input type="number" name="armor_quant" value=0></td>
-				<td>Armor price: <input type="number" step= 0.01 name="armor_price" value=0.0></td>
-			
+				<td>Armor quantity: <input type="number" name="armor_quant" value=<?php echo isset($stats['armor_quant'])?$stats['armor_quant']:0; ?>></td>
+				<td>Armor price: <input type="number" step= 0.01 name="armor_price" value=<?php echo isset($stats['armor_price'])?$stats['armor_price']:0.0; ?>></td>
 			</tr>
 	
 		</table>
@@ -83,6 +108,16 @@
 	
 	</form>
 
+	<a href=
+	
+	<?php 
+		if($error){
+		
+			echo "Some value is invalid. Recheck inputs.";
+			
+		} 
+	?>
+	
 </body>
 </html>
 
--- private/functions.php
+++ private/functions.php
@@ -2,15 +2,29 @@
 
 	// Application-wide functions defined here
 	function ht($string=""){
-		
 		htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
-		
+	}
+	
+	function h($string=""){
+		htmlspecialchars($string);
 	}
 	
 	function u($string=""){
-		
 		rawurlencode($string);
-		
 	}
 
+	function get_post($post_value, $submitted_value){
+		return isset($_POST[$post_value]) && $submitted_value?$_POST[$post_value]:0;
+	}
+	
+	function validate_stats($statarray) {
+		
+		foreach ($statarray as $val)
+			if($val <= 0)
+				return false;
+		
+		return true;
+		
+	}
+	
 ?>
